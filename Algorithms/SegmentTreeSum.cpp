dcf65ad942418e77335431a51faf03ebff8cc2c9
Anirudh S <anirudh24seven@gmail.com>
2013-04-09 09:17:14 +0530

    Segment tree sum [Iteration: 1]
    Find sum of contiguous sequence using segment tree
    
    [Iteration: 1]

---
+++ Algorithms/SegmentTreeSum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Take input array inputArray
+	// Construct segment tree out of it
+	// Get the sum of elements a...b
+	// Update a value k
+	// Get the sum of elements c...d
+}
