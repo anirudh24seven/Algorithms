0a3d577b77a8d1339c86a309b6cec79573768767
Anirudh S <anirudh24seven@gmail.com>
2013-04-22 22:34:31 +0530

    Closer to working algorithm

--- Algorithms/SegmentTreeSum.cpp
+++ Algorithms/SegmentTreeSum.cpp
@@ -3,60 +3,66 @@
 #include <math.h>
 using namespace std;
 
-int *constructSegmentTreewrapper (int inputArray[], int inputArraySize);
 int constructSegmentTreeCore (int inputArray[], int startPosition, int endPosition, int* segmentTree, int currentNode);
-int getSum (int *segmentTree, int segmentTreeSize, int a, int b);
+int getSumWrapper (int *segmentTree, int segmentTreeSize, int a, int b);
+int getSumCore (int *segmentTree, int startPosition, int endPosition, int a, int b, int index);
 void updateSegmentTreeWrapper (int inputArray[], int *segmentTree, int segmentTreeSize, int k, int value);
 void updateSegmentTreeCore(int *segmentTree, int startPosition, int endPosition, int k, int difference, int index);
 
 int main() {
-	int i, n;
-	cin>>n;
+    int i, n;
+    cin>>n;
 
-	vector<int> inputVector (n);
-	int* inputArray = new int[n];
-	// Take input vector inputVector
-	for(i=0; i<n; i++) {
-		cin>>inputArray[i];
-	}
+    vector<int> inputVector (n);
+    int* inputArray = new int[n];
+    // Take input vector inputVector
+    for(i=0; i<n; i++) {
+	    cin>>inputArray[i];
+    }
 
-	//int* inputArray = &inputVector[0];	
+    //int* inputArray = &inputVector[0];
+    int inputArraySize = n;
+    // Construct segment tree out of it
+    // Height max of segment tree is log2 (size)
+    int height = (int)(ceil(log(inputArraySize)/log(2)));
+    int level = height+1;
 
-	// Construct segment tree out of it
-	int *segmentTree = constructSegmentTreeWrapper(inputArray, n);
-	int segmentTreeSize = sizeof(segmentTree)/sizeof(segmentTree[0]); 
+    // Size max of segment tree is sigma(1...height)(n) = 2^height - 1
+    int maxSize = (int)pow(2,level) - 1;
+    cout<<"Size: "<<maxSize<<endl;
 
-	// Get the sum of elements in range a...b
-	int a, b;
-	cin>>a>>b;
-	int sum = getSum(segmentTree, segmentTreeSize, a, b);
-	cout<<"Sum: "<<sum<<endl;
+    int *segmentTree = new int[maxSize];
 
-	// Update a value k
-	int k, value;
-	cin>>k>>value;
+    constructSegmentTreeCore (inputArray, 0, inputArraySize-1, segmentTree, 0);
 
-	updateSegmentTreeWrapper(inputArray, segmentTree, n, k, value);
+    int segmentTreeSize = sizeof(segmentTree)/sizeof(segmentTree[0]);
 
-	// Get the sum of elements in range a...b
-	cin>>a>>b;
-	sum = getSum(segmentTree, segmentTreeSize, a, b);
-	cout<<"Sum: "<<sum<<endl;
-}
+    for (i=0; i<maxSize; i++) {	  
+      cout<<segmentTree[i]<<" ";	  
+    }
+    cout<<endl;
+    
+    for(i=0; i<n; i++) {
+      cout<<inputArray[i]<<" ";
+    }
+    cout<<endl;
+    
+    // Get the sum of elements in range a...b
+    int a, b;
+    cin>>a>>b;
+    int sum = getSumWrapper(segmentTree, maxSize, a, b);
+    cout<<"Sum: "<<sum<<endl;
 
+    // Update a value k
+    int k, value;
+    cin>>k>>value;
 
-int* constructSegmentTreeWrapper (int inputArray[], int inputArraySize) {
-	// Height max of segment tree is log2 (size)
-	int height = (int)(ceil(log(inputArraySize)/log(2)));
+    updateSegmentTreeWrapper(inputArray, segmentTree, n, k, value);
 
-	// Size max of segment tree is sigma(1...height)(n) = 2^height - 1 
-	int maxSize = (int)pow(2,height) - 1;
-
-	int *segmentTree = new int[maxSize];
-
-	constructSegmentTreeCore (inputArray, 0, inputArraySize-1, segmentTree, 0);
-	
-	return inputArray;
+    // Get the sum of elements in range a...b
+    cin>>a>>b;
+    sum = getSumWrapper(segmentTree, maxSize, a, b);
+    cout<<"Sum: "<<sum<<endl;
 }
 
 int constructSegmentTreeCore (int inputArray[], int startPosition, int endPosition, int* segmentTree, int currentNode) {
@@ -67,14 +73,34 @@ int constructSegmentTreeCore (int inputArray[], int startPosition, int endPositi
 	}
 
 	// Recursively fill left and right subtrees and store sum in this node
-	int middlePosition = (startPosition + endPosition)/2;
-	segmentTree[currentNode] = constructSegmentTreeCore(inputArray, startPosition, middlePosition, segmentTree, currentNode*2+1) + constructSegmentTreeCore(inputArray, middlePosition+1, endPosition, segmentTree, currentNode*2+2);
-
+	int middlePosition = startPosition + (endPosition-startPosition)/2;
+	segmentTree[currentNode] = constructSegmentTreeCore(inputArray, startPosition, middlePosition-1, segmentTree, currentNode*2+1) + constructSegmentTreeCore(inputArray, middlePosition, endPosition, segmentTree, currentNode*2+2);
+	
 	return segmentTree[currentNode];
 }
 
-int getSum (int *segmentTree, int segmentTreeSize, int a, int b) {
-	return 0;
+int getSumWrapper (int *segmentTree, int segmentTreeSize, int a, int b) {
+	if (a<0 || b>segmentTreeSize-1 || a>b) {
+	  cout<<a<<" "<<b<<" "<<segmentTreeSize;
+	  cout<<"Invalid input"<<endl;
+	  return -1;
+	}
+	
+	return getSumCore(segmentTree, 0, segmentTreeSize-1, a, b, 0);
+}
+
+int getSumCore (int *segmentTree, int startPosition, int endPosition, int a, int b, int index) {
+	if (a<=startPosition && b>=endPosition) {
+	  return segmentTree[index];
+	}
+	
+	if (a > endPosition || b < startPosition) {
+	  return 0;
+	}
+	
+	int middlePosition = startPosition + (endPosition-startPosition)/2;
+	
+	return getSumCore(segmentTree, startPosition, middlePosition-1, a, b, 2*index+1) + getSumCore(segmentTree, middlePosition, endPosition, a, b, 2*index+2);
 }
 
 void updateSegmentTreeWrapper (int inputArray[], int *segmentTree, int segmentTreeSize, int k, int value) {
@@ -100,8 +126,8 @@ void updateSegmentTreeCore(int *segmentTree, int startPosition, int endPosition,
 	segmentTree[index] = segmentTree[index] + difference;
 
 	if(startPosition != endPosition) {
-		int middlePosition = (startPosition + endPosition)/2;
-		updateSegmentTreeCore(segmentTree, startPosition, middlePosition, k, difference, 2*index+1);
-		updateSegmentTreeCore(segmentTree, middlePosition+1, endPosition, k, difference, 2*index+2);
+		int middlePosition = startPosition + (endPosition-startPosition)/2;
+		updateSegmentTreeCore(segmentTree, startPosition, middlePosition-1, k, difference, 2*index+1);
+		updateSegmentTreeCore(segmentTree, middlePosition, endPosition, k, difference, 2*index+2);
 	}
 }
